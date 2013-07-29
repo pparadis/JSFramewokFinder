1a7381901044e2188b0df09913095aaabe16b6e6
Pascal Paradis <pascal.paradis@gmail.com>
2013-07-29 19:58:00 -0400

    Ajout de code

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
     console.log($('h1').text());
+    
+    $.ajax("https://github.com/search?q=created%3A%3E2013-07-29+language%3Ajavascript+framework+&type=Repositories&ref=searchresults")
+        .done(function() { 
+            $(this).addClass("done");
+    });
+    
+    
+    $.ajax({
+        url: "test.html",
+        context: document.body
+    }).done(function() {
+        $("#Result")
+            .addClass("done");
+    });
 });
