5034531fb2b66f633d57eaf544ef0081fe562099
Pascal Paradis <pascal.paradis@gmail.com>
2013-07-29 20:03:40 -0400

    On avance!

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 $(document).ready(function(){
     console.log($('h1').text());
-    
-    $.ajax("https://github.com/search?q=created%3A%3E2013-07-29+language%3Ajavascript+framework+&type=Repositories&ref=searchresults")
-        .done(function() { 
-            $(this).addClass("done");
-    });
-    
-    
+       
     $.ajax({
-        url: "test.html",
+        url: "https://github.com/search?q=created%3A%3E2013-07-29+language%3Ajavascript+framework+&type=Repositories&ref=searchresults",
         context: document.body
     }).done(function() {
         $("#Result")
